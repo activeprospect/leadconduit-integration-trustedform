afc4c3834a7952841f53f14b904551b72a01df42
johnrb2 <john.bench@activeprospect.com>
2022-10-25 12:48:40 -0400

    rename consent_plus_data.js to consent_plus_insights.jst

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = {
   outbound: {
     claim: require('./lib/claim'),
     consent: require('./lib/consent'),
-    consent_plus_data: require('./lib/consent_plus_data'),
+    consent_plus_insights: require('./lib/consent_plus_insights'),
     data_service: require('./lib/data_service'),
     decision_service_ping: require('./lib/data_service'),
     decision_service_post: require('./lib/data_service')
